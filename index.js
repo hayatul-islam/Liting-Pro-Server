5155ead2c987aea10969abdfbf9aa1ee2fe9d74c
hayatulemon <hayatulemon340@gmail.com>
2022-01-13 21:17:20 +0600

    image upload limit 50mb

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 // image upload
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 1000000, limit: '500mb' }));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9gms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
