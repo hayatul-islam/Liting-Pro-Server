b18769bb5dc8a2a9acc01b5ccbae507b9674544a
hayatulemon <hayatulemon340@gmail.com>
2022-01-13 22:26:49 +0600

    img upload body parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 4040;
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 // image upload
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9gms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
