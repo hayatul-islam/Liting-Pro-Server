c71f35e9e83dc5be5728ae2e13ba510de3f0154a
hayatulemon <hayatulemon340@gmail.com>
2022-01-13 18:13:42 +0600

    image upload code

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-// image upload
-// app.use(express.json({ limit: '25mb' }));
-// app.use(express.urlencoded({ limit: '25mb', extended: true }));
-
 const app = express();
 const port = process.env.PORT || 4040;
 
 app.use(cors());
 app.use(express.json());
+
+// image upload
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9gms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
